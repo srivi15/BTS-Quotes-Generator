46ef4584ed9c9c233f0951ada38d9c31149109ae
Srividhya Ranganathan <54398008+srivi15@users.noreply.github.com>
2023-05-18 18:41:17 -0500

    added files

---
+++ script.js
@@ -0,0 +1,37 @@
+const btsQuote = document.querySelector(".quote"),
+sourceName = document.querySelector(".source .name"),
+newQuote = document.querySelector("button"),
+copybutton = document.querySelector("copy"),
+twitter = document.querySelector("twitter");
+
+let quotesData;
+
+fetch("bts_quotes.json")
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data);
+        quotesData = data;
+        //btsQuote.innerText = data[4].quotes;
+        //sourceName.innerText = data[4].source;
+    }).catch(error => console.log(error));
+
+function randomBTSQuote(){
+    if(quotesData){
+        const index = Math.floor(Math.random() * quotesData.length);
+        const randomQuote = quotesData[index].Quotes;
+        const rqSource = quotesData[index].Source;
+        btsQuote.textContent = randomQuote;
+        sourceName.textContent = rqSource;
+    }
+};
+
+newQuote.addEventListener("click", randomBTSQuote);
+
+twitter.addEventListener("click", () => {
+    let tweetURL = `https://twitter.com/intent/tweet?url=${quotesData[index].Quotes}`;
+    window.open(tweetURL, "_blank");
+});
+
+copybutton.addEventListener("click", () => {
+    navigator.clipboard.writeText(quotesData[index].Quotes);
+});
